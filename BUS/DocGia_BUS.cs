1b21e56a56ccb00cc1cd5d937a52c3c980729d73
Sơn Lê <ltsson1111@gmail.com>
2020-12-04 23:25:28 +0700

    fvf

--- BUS/DocGia_BUS.cs
+++ BUS/DocGia_BUS.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using DAL;
+using DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BUS
 {
-    class DocGia_BUS
+    public class DocGia_BUS
     {
-    }
+        DocGia_DAL dalDocGia = new DocGia_DAL();
+        public DataTable getDocGia()
+        {
+            return dalDocGia.getDocGia();
+        }
+        public bool InsertDocGia(DocGia_DTO pm)
+        {
+            return dalDocGia.InsertDocGia(pm);
+        }
+        public bool UpdateDocGia(DocGia_DTO pm)
+        {
+            return dalDocGia.UpdateDocGia(pm);
+        }
+        public bool DeleteDocGia(string MaDocGia)
+        {
+            return dalDocGia.DeleteDocGia(MaDocGia);
+        }
+        public DataTable SearchDocGia(string HoTen)
+        {
+            return dalDocGia.SearchDocGia(HoTen);
+        }
 }
--- DAL/DocGia_DAL.cs
+++ DAL/DocGia_DAL.cs
@@ -9,9 +9,9 @@ using DTO;
 
 namespace DAL
 {
-    class DocGia_DAL : DBConnect
+    public class DocGia_DAL : DBConnect
     {
-        public DataTable getKhach()
+        public DataTable getDocGia()
         {
             try
             {
@@ -20,9 +20,110 @@ namespace DAL
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.CommandText = "DanhSachDocGia";
                 cmd.Connection = _conn;
-                DataTable dtKhach = new DataTable();
-                dtKhach.Load(cmd.ExecuteReader());
-                return dtKhach;
+                DataTable dtDocGia = new DataTable();
+                dtDocGia.Load(cmd.ExecuteReader());
+                return dtDocGia;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+
+        public bool InsertDocGia(DocGia_DTO pm)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "INSERTPHIEUMUON";
+                cmd.Parameters.AddWithValue("MaDocGia", pm.MADOCGIA);
+                cmd.Parameters.AddWithValue("HoTen", pm.HOTEN);
+                cmd.Parameters.AddWithValue("GioiTinh", pm.GIOITINH);
+                cmd.Parameters.AddWithValue("NamSinh", pm.NAMSINH);
+                cmd.Parameters.AddWithValue("DiaChi", pm.DIACHI);
+                cmd.Parameters.AddWithValue("SoDienThoai", pm.SODIENTHOAI);
+                cmd.Parameters.AddWithValue("Email", pm.EMAIL);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                _conn.Close();
+            }
+
+        }
+        public bool UpdateDocGia(DocGia_DTO pm)
+        {
+            try
+            {
+                _conn.Open();
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = _conn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "INSERTPHIEUMUON";
+                cmd.Parameters.AddWithValue("MaDocGia", pm.MADOCGIA);
+                cmd.Parameters.AddWithValue("HoTen", pm.HOTEN);
+                cmd.Parameters.AddWithValue("GioiTinh", pm.GIOITINH);
+                cmd.Parameters.AddWithValue("NamSinh", pm.NAMSINH);
+                cmd.Parameters.AddWithValue("DiaChi", pm.DIACHI);
+                cmd.Parameters.AddWithValue("SoDienThoai", pm.SODIENTHOAI);
+                cmd.Parameters.AddWithValue("Email", pm.EMAIL);
+
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+        }
+        public bool DeleteDocGia(string MaDocGia)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "DeleteDocGia";
+                cmd.Parameters.AddWithValue("MADOCGIA", MaDocGia);
+                cmd.Connection = _conn;
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return false;
+        }
+        public DataTable SearchDocGia(string HoTen)
+        {
+            try
+            {
+                _conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SearchDocGia";
+                cmd.Parameters.AddWithValue("HOTEN", HoTen);
+                cmd.Connection = _conn;
+                DataTable dtDocGia = new DataTable();
+                dtDocGia.Load(cmd.ExecuteReader());
+                return dtDocGia;
             }
             finally
             {
